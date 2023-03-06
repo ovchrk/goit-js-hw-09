dfd965e028ab1fc07f6a61d4e477139d2cf4592c
ovchrk <113932934+ovchrk@users.noreply.github.com>
2023-03-06 19:24:12 +0200

    task01 done

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const bodyRef = document.querySelector('body');
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
+console.log(stopBtnRef);
+
+startBtnRef.addEventListener('click', startBtnClickHandler);
+stopBtnRef.addEventListener('click', stopBtnClickHandler);
+
+function startBtnClickHandler(evt) {
+  timerId = setInterval(() => {
+    console.log(`I love async JS!  ${Math.random()}`);
+    bodyRef.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+
+  startBtnRef.setAttribute('disabled', 'disabled');
+}
+
+function stopBtnClickHandler(evt) {
+  clearInterval(timerId);
+  startBtnRef.removeAttribute('disabled');
+}
