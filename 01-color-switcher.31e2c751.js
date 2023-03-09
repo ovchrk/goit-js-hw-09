d1ad55d9a0e1a61cae3cde3c3f1e254fbf45a8f1
ovchrk <113932934+ovchrk@users.noreply.github.com>
2023-03-09 20:09:25 +0000

    Deploying to gh-pages from @ ovchrk/goit-js-hw-09@645b998a119d95a206cf2f273cf994460829da25 🚀

---
+++ 01-color-switcher.31e2c751.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");console.log(o),e.addEventListener("click",(function(o){timerId=setInterval((()=>{console.log(`I love async JS!  ${Math.random()}`),t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.setAttribute("disabled","")})),o.addEventListener("click",(function(t){clearInterval(timerId),e.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.31e2c751.js.map
---
+++ 01-color-switcher.e298f869.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");console.log(o),e.addEventListener("click",(function(o){timerId=setInterval((function(){console.log("I love async JS!  ".concat(Math.random())),t.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3),e.setAttribute("disabled","")})),o.addEventListener("click",(function(t){clearInterval(timerId),e.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.e298f869.js.map
