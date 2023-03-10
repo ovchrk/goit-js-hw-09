3787876edc746b34634942c54c52ff75a1d3d629
ovchrk <113932934+ovchrk@users.noreply.github.com>
2023-03-10 12:11:42 +0000

    Deploying to gh-pages from @ ovchrk/goit-js-hw-09@1e4b7def5d210b58f55f34970fd3867bef047a76 🚀

---
+++ 01-color-switcher.b6c71f71.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");e.addEventListener("click",(function(o){n=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0})),o.addEventListener("click",(function(t){clearInterval(n),e.removeAttribute("disabled")}));let n=null;
+//# sourceMappingURL=01-color-switcher.b6c71f71.js.map
